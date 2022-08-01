08b74b97d27096e5ed8b1716bcc9054d8b3329d7
Badea-pro <79918603+Badea-pro@users.noreply.github.com>
2022-08-02 01:57:07 +0300

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,42 +1,51 @@
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
+const passport = require('passport');
+const _ = require('lodash');
 
-var User = mongoose.model('User');
+const User = mongoose.model('User');
 
-passport.use(
-    new localStrategy({ usernameField: 'email' },
-    // Email chack
-        (username, password, done) => {
-            User.findOne({ email: username.toLowerCase() },
-                (err, user) => {
-                    if (err)
-                        return done(err);
-                    // unknown user
-                    else if (!user)
-                        return done(null, false, { message: 'Email is not registered' });
-                    // wrong password
-                    else if (!user.verifyPassword(password))
-                        {
-                            // The track login attempts 
-                            User.findOneAndUpdate({email : username.toLowerCase()}, {$inc:{loginAttempts:1}}, {new:true}).then(a => {console.log(a); 
-                                if(a?.loginAttempts > 3){
-                                    return done(null, false, { message: 'Pleas contact the admin by : Badeakhalboos@gmail.com.' });
-                                }
-                                // If there a wrong password 
-                                else{return done(null, false, { message: 'Wrong password.' });}
-                            })
-                    }
-                    // authentication succeeded
-                    else {
-                        // If user has more than 3 login attempts
-                        if (user?.loginAttempts > 3) {
-                            return done(null, false, { message: 'Pleas contact the admin by : Badeakhalboos@gmail.com.' });
-                        }
-                        return done(null, user);
-                    }
-                        
-                    
-                });
-        })
-);
+// If there a Duplicate email
+
+module.exports.register = (req, res, next) => {
+    var user = new User();
+    user.fullName = req.body.fullName;
+    // Save email as lower case
+    user.email = req.body.email.toLowerCase();
+    user.password = req.body.password;
+    user.save((err, doc) => {
+        if (!err)
+            res.send(doc);
+        else {
+            if (err.code == 11000)
+                res.status(422).send(['Duplicate email adrress found.']);
+            else
+                return next(err);
+        }
+
+    });
+}
+
+// The call for passport authentication
+
+module.exports.authenticate = (req, res, next) => {
+    // call for passport authentication
+    passport.authenticate('local', (err, user, info) => {       
+        // error from passport middleware
+        if (err) return res.status(400).json(err);
+        // registered user
+        else if (user) return res.status(200).json({ "token": user.generateJwt() });
+        // unknown user or wrong password
+        else return res.status(404).json(info);
+    })(req, res);
+}
+
+module.exports.userProfile = (req, res, next) =>{
+    User.findOne({ _id: req._id },
+        (err, user) => {
+            if (!user)
+                return res.status(404).json({ status: false, message: 'User record not found.' });
+            else
+                return res.status(200).json({ status: true, user : _.pick(user,['fullName','email']) });
+        }
+    );
+}
